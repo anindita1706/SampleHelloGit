ce099f3ecfc999330ddf736a047bcce2854e425b
anindita1706 <111552384+anindita1706@users.noreply.github.com>
2022-09-29 01:03:40 +0530

    Add files via upload

---
+++ SampleHelloGit/src/test/java/SampleHelloGit/TestHelloGit.java
@@ -0,0 +1,13 @@
+package SampleHelloGit;
+
+import org.testng.annotations.Test;
+
+public class TestHelloGit {
+
+	@Test 
+	public void testHelloGit() {
+		
+		System.out.println("Hello, Git");
+	
+	}
+}
